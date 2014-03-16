4c79881c8d831bb53049fb6be41240bc807d5f35
Caleb Xu <caleb@caleb.local>
2014-03-16 19:47:15 -0400

    Remove function definition

--- brew-hangover.rb
+++ brew-hangover.rb
@@ -1,8 +1,6 @@
-def hangover
-  ohai 'brew update'
-  system 'brew update'
-  ohai 'brew upgrade'
-  system 'brew upgrade'
-  ohai 'rm -rf /Library/Caches/Homebrew'
-  system 'rm -rf /Library/Caches/Homebrew'
-end
+ohai 'brew update'
+system 'brew update'
+ohai 'brew upgrade'
+system 'brew upgrade'
+ohai 'rm -rf /Library/Caches/Homebrew'
+system 'rm -rf /Library/Caches/Homebrew'
